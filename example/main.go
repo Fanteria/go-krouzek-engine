6869fe2baef9c511ae44def61595782b9cc4c0d6
Jiří Alexandrovič <jiri.alexandrovic@aveco.com>
2026-04-02 23:25:46 +0200

    Add animated tree.

--- example/main.go
+++ example/main.go
@@ -16,20 +16,29 @@ func main() {
 	gke.NastavPozici(blok, 100.0, 150.0)
 	gke.NastavBlokovani(blok, true)
 
+	blok_cislo_n := 0.0
+	for blok_cislo_n <= 20 {
+		blok1 := gke.PridejBlokSVyrezem("./obrazky/bloky.png", gke.Vyrez{X1: 0, Y1: 0, X2: 32, Y2: 32})
+		gke.NastavPozici(blok1, blok_cislo_n*32, 400)
+		gke.NastavBlokovani(blok1, true)
+		blok_cislo_n += 1
+	}
+
 	blok_s_vyrezem := gke.PridejBlokSVyrezem("./obrazky/knight.png", gke.Vyrez{X1: 34, Y1: 5, X2: 49, Y2: 26})
 	gke.NastavPozici(blok_s_vyrezem, 150.0, 150.0)
 
+	var animace []gke.Vyrez
+	for i := 0; i < 37; i += 1 {
+		animace = append(animace, gke.Vyrez{X1: i * 64, Y1: 0, X2: (i + 1) * 64, Y2: 64})
+	}
 	animovany_blok := gke.PrijdejAnimovanyBlok(
-		"./obrazky/knight.png",
-		0.1,
-		gke.Vyrez{X1: 34, Y1: 5, X2: 49, Y2: 24},
-		gke.Vyrez{X1: 50, Y1: 5, X2: 65, Y2: 24},
-		gke.Vyrez{X1: 66, Y1: 5, X2: 81, Y2: 24},
-		gke.Vyrez{X1: 82, Y1: 5, X2: 97, Y2: 24},
+		"./obrazky/tree_animated.png",
+		0.08,
+		animace...,
 	)
-	gke.NastavPozici(animovany_blok, 250.0, 150.0)
-	gke.NastavZvetseni(animovany_blok, 2)
-	gke.NastavBlokovani(animovany_blok, true)
+	gke.NastavPozici(animovany_blok, 250.0, 208.0)
+	gke.NastavZvetseni(animovany_blok, 3)
+	gke.NastavBlokovani(animovany_blok, false)
 
 	hratelna_postava := gke.PrijdejHratelnouPostavu(
 		"./obrazky/knight.png",
